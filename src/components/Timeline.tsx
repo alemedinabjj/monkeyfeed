bb379f8258d5818c6d9f431ceb077547be48f1d4
Alexandre Medina <alesurf13@gmail.com>
2023-09-29 23:35:10 -0300

    integration backend

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -5,46 +5,18 @@ import { Post } from "./Post";
 import { Header } from "./Header";
 import { useQuery } from "react-query";
 import { api } from "../lib/api";
-const posts = [
-  {
-    id: 1,
-    author: {
-      avatar_url: "https://github.com/alemedinabjj.png",
-      name: "Alexandre Medina",
-      role: "Web Developer"
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋',},
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
-      { type: 'paragraph', content: 'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'link', content: 'jane.design/doctor-care'},
-    ],
-    publishedAt: new Date('2022-08-09 05:00:00'),
-  },
-  {
-    id: 2,
-    author: {
-      avatar_url: "https://github.com/maykbrito.png",
-      name: "Mayk Brito",
-      role: "Instrutor at Rocketseat"
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋',},
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz'},
-      { type: 'paragraph', content: 'no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'link', content: 'jane.design/doctor-care'},
-    ],
-    publishedAt: new Date('2022-08-07 20:00:00'),
-    
-  },
-]
+
 
 export function Timeline() {
   const { data } = useQuery(['posts'], async () => {
     const res = await api.get('/posts/all-posts')
     console.log(res)
     return res.data
-  })
+  },
+  {
+    refetchInterval: 1000 * 60 // 1 minuto
+  }
+  )
 
   return (
   <>
