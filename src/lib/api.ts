474fff651fb8758f243ad07763365a4f44dd0440
Alexandre Medina <alesurf13@gmail.com>
2023-09-29 23:24:53 -0300

    integration backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://ec2-13-59-145-7.us-east-2.compute.amazonaws.com',
+  baseURL: 'https://api-monkey.onrender.com/',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('@token-monkey')}`
   }
